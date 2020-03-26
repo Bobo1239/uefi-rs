ead8770c8bf802497c84b7dbae0f37509097e464
Boris-Chengbiao Zhou <bobo1239@web.de>
2020-03-26 15:39:55 +0100

    try

--- uefi-test-runner/src/proto/console/serial.rs
+++ uefi-test-runner/src/proto/console/serial.rs
@@ -4,49 +4,49 @@ use uefi::table::boot::BootServices;
 
 pub fn test(bt: &BootServices) {
     info!("Running serial protocol test");
-    if let Ok(serial) = bt.locate_protocol::<Serial>() {
-        let serial = serial.expect("Warnings encountered while opening serial protocol");
-        let serial = unsafe { &mut *serial.get() };
+    // if let Ok(serial) = bt.locate_protocol::<Serial>() {
+    //     let serial = serial.expect("Warnings encountered while opening serial protocol");
+    //     let serial = unsafe { &mut *serial.get() };
 
-        let old_ctrl_bits = serial
-            .get_control_bits()
-            .expect_success("Failed to get device control bits");
-        let mut ctrl_bits = ControlBits::empty();
+    //     let old_ctrl_bits = serial
+    //         .get_control_bits()
+    //         .expect_success("Failed to get device control bits");
+    //     let mut ctrl_bits = ControlBits::empty();
 
-        // For the purposes of testing, we're _not_ going to implement
-        // software flow control.
-        ctrl_bits |= ControlBits::HARDWARE_FLOW_CONTROL_ENABLE;
+    //     // For the purposes of testing, we're _not_ going to implement
+    //     // software flow control.
+    //     ctrl_bits |= ControlBits::HARDWARE_FLOW_CONTROL_ENABLE;
 
-        // Use a loop back device for testing.
-        ctrl_bits |= ControlBits::SOFTWARE_LOOPBACK_ENABLE;
+    //     // Use a loop back device for testing.
+    //     ctrl_bits |= ControlBits::SOFTWARE_LOOPBACK_ENABLE;
 
-        serial
-            .set_control_bits(ctrl_bits)
-            .expect_success("Failed to set device control bits");
+    //     serial
+    //         .set_control_bits(ctrl_bits)
+    //         .expect_success("Failed to set device control bits");
 
-        // Keep this message short, we need it to fit in the FIFO.
-        const OUTPUT: &[u8] = b"Hello world!";
-        const MSG_LEN: usize = OUTPUT.len();
+    //     // Keep this message short, we need it to fit in the FIFO.
+    //     const OUTPUT: &[u8] = b"Hello world!";
+    //     const MSG_LEN: usize = OUTPUT.len();
 
-        serial
-            .write(OUTPUT)
-            .expect_success("Failed to write to serial port");
+    //     serial
+    //         .write(OUTPUT)
+    //         .expect_success("Failed to write to serial port");
 
-        let mut input = [0u8; MSG_LEN];
-        serial
-            .read(&mut input)
-            .expect_success("Failed to read from serial port");
+    //     let mut input = [0u8; MSG_LEN];
+    //     serial
+    //         .read(&mut input)
+    //         .expect_success("Failed to read from serial port");
 
-        assert_eq!(&OUTPUT[..], &input[..]);
+    //     assert_eq!(&OUTPUT[..], &input[..]);
 
-        // Clean up after ourselves
-        serial
-            .reset()
-            .expect_success("Could not reset the serial device");
-        serial
-            .set_control_bits(old_ctrl_bits & ControlBits::SETTABLE)
-            .expect_success("Could not restore the serial device state");
-    } else {
-        warn!("No serial device found");
-    }
+    //     // Clean up after ourselves
+    //     serial
+    //         .reset()
+    //         .expect_success("Could not reset the serial device");
+    //     serial
+    //         .set_control_bits(old_ctrl_bits & ControlBits::SETTABLE)
+    //         .expect_success("Could not restore the serial device state");
+    // } else {
+    //     warn!("No serial device found");
+    // }
 }
